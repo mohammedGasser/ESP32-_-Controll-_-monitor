59246c3fa7f225a8f2d43fac46c9a0a9ca43fba8
mohammedGasser <mohamedgassermohamede@gmail.com>
2026-07-04 23:04:20 +0300

    Update Dashboard UI Dashboards js

---
+++ js/Dashboarrd.js
@@ -0,0 +1,45 @@
+ function openDashboard(id){
+
+    var xhttp = new XMLHttpRequest();
+
+    xhttp.open("GET","/dashboard?id="+id,true);
+
+    xhttp.send();
+
+}
+
+function getSpectrum() {
+
+    var xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function () {
+
+        if (this.readyState == 4 && this.status == 200) {
+
+            document.getElementById("value").innerHTML = this.responseText;
+
+        }
+
+    };
+
+    xhttp.open("GET", "/spectrum", true);
+
+    xhttp.send();
+}
+
+getSpectrum();
+// كرر القراءة كل ثانية
+setInterval(getSpectrum, 1000);
+
+
+function sendName() {
+
+    var name = document.getElementById("txt").value;
+
+    var xhttp = new XMLHttpRequest();
+
+    xhttp.open("GET", "/name?user=" + encodeURIComponent(name), true);
+
+    xhttp.send();
+
+}
